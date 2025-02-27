267ce4b96cebaa668f68fc0346d860d7930dbc58
Oleg Kosmakov <1533952+kosmakoff@users.noreply.github.com>
2025-02-27 22:37:51 +0100

    Handle graceful shutdown of the daemon

--- lenovo-kb-lightd/src/main.rs
+++ lenovo-kb-lightd/src/main.rs
@@ -1,8 +1,14 @@
 use common::find_hidraw_device_name;
+use common::led_config::LedConfig;
 use evdev::EventSummary::{Key, Misc};
 use evdev::{Device, KeyCode, MiscCode};
+use nix::poll::{poll, PollFd, PollFlags};
+use signal_hook::consts::SIGTERM;
+use std::os::fd::{AsRawFd, BorrowedFd};
 use std::process::ExitCode;
-use common::led_config::LedConfig;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use nix::errno::Errno;
 
 const FN_SPACE: i32 = 0xc0050;
 const DEVICE: &str = "/dev/input/by-id/usb-ITE_Tech._Inc._ITE_Device_8910_-event-kbd";
@@ -27,6 +33,15 @@ fn main() -> ExitCode {
 
     let mut is_on = config.is_on;
 
+    let stop_flag = Arc::new(AtomicBool::new(false));
+    match signal_hook::flag::register(SIGTERM, Arc::clone(&stop_flag)) {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!("Failed to register signal handler: {}", err);
+            return ExitCode::from(1);
+        }
+    };
+
     let mut device = match Device::open(DEVICE) {
         Ok(device) => device,
         Err(err) => {
@@ -35,9 +50,29 @@ fn main() -> ExitCode {
         }
     };
 
+    let raw_fd = device.as_raw_fd();
+    let fd = unsafe { BorrowedFd::borrow_raw(raw_fd) };
+    let mut poll_fd = [PollFd::new(fd, PollFlags::POLLIN)];
+
     let mut is_pressed = false;
 
-    loop {
+    while !stop_flag.load(Ordering::Relaxed) {
+        match poll(&mut poll_fd, 100u16) {
+            Ok(0) => {
+                continue;
+            }
+            Ok(_) => {}
+            Err(Errno::EINTR) => {
+                println!("Interrupt detected");
+                break;
+            }
+            Err(err) => {
+                let error_description = err.desc();
+                eprintln!("Failed to poll for device events: {}", error_description);
+                return ExitCode::from(1);
+            }
+        }
+
         for event in device.fetch_events().unwrap() {
             let mut should_switch = false;
             match event.destructure() {
@@ -64,7 +99,7 @@ fn main() -> ExitCode {
                     println!("Turning the LEDs on");
                     common::turn_backlight_on(&device_name)
                 };
-                
+
                 config.is_on = is_on;
                 match config.save() {
                     Ok(_) => {}
@@ -84,4 +119,7 @@ fn main() -> ExitCode {
             }
         }
     }
+
+    println!("Exiting gracefully");
+    ExitCode::SUCCESS
 }
